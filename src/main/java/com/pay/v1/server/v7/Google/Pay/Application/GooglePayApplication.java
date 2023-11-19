e78c65d215c882a4ee3ac348994fe22ffb3954ed
himanshu <singourhimanshu3@gmail.com>
2023-11-19 23:25:14 +0530

    new

---
+++ src/main/java/com/pay/v1/server/v7/Google/Pay/Application/GooglePayApplication.java
@@ -0,0 +1,13 @@
+package com.pay.v1.server.v7.Google.Pay.Application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GooglePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GooglePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pay/v1/server/v7/Google/Pay/Application/GooglePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.v1.server.v7.Google.Pay.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GooglePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
