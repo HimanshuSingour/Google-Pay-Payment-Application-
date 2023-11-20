a2b8a0b4c0278a0e0ccd4e8ac25b3739fd1b35f6
himanshu <singourhimanshu3@gmail.com>
2023-11-20 09:47:36 +0530

    Adding Security

--- src/main/java/com/pay/v1/server/v7/Google/Pay/Application/repository/UserRepositories.java
+++ src/main/java/com/pay/v1/server/v7/Google/Pay/Application/repository/UserRepositories.java
@@ -2,13 +2,17 @@ package com.pay.v1.server.v7.Google.Pay.Application.repository;
 
 import com.pay.v1.server.v7.Google.Pay.Application.entity.UserInformation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepositories extends JpaRepository<UserInformation, String> {
+
+    @Query("SELECT u FROM UserInformation u WHERE u.email =:email")
     Optional<UserInformation> existByEmail(String email);
 
+    @Query("SELECT u FROM UserInformation u WHERE u.phoneNumber =:phoneNumber")
     Optional<UserInformation> existByPhoneNumber(String phoneNumber);
 }
