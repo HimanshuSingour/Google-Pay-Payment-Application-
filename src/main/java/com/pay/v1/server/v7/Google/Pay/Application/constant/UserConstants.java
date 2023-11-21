cfbbdf3ccd71539e614c7e46a2679a45d7572719
himanshu <singourhimanshu3@gmail.com>
2023-11-21 19:53:17 +0530

    Adding User Get Login Details Login

--- src/main/java/com/pay/v1/server/v7/Google/Pay/Application/constant/UserConstants.java
+++ src/main/java/com/pay/v1/server/v7/Google/Pay/Application/constant/UserConstants.java
@@ -7,13 +7,10 @@ public class UserConstants {
 
     public static final String ALL_FIELD_REQUIRED = "Completion of all fields is mandatory for submission, thank you";
     public static final String SUCCESSFULLY_REGISTERED = "Your account have been created successfully";
-
     public static final String DUPLICATE_EMAIL_EXIST = "This email address is already registered.";
     public static final String DUPLICATE_PHONE_NUMBER_EXIST = "This phone number is already in use.";
     public static final String SUCCESS_ONE = "SUCCESS";
-
     public static final String DETAIL_NOT_FOUND = "You haven't registered your account yet. there is no account found with this details";
-
     public static final String USER_LOGIN_INFO = "You can log in using your first name as username and the provided password.";
 
 
